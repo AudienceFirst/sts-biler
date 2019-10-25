c96595b5519f58733a91542c80b0f74c9b4e1198
MAKE-jonroize <jon@makedevelopment.com>
2019-10-25 17:43:59 +0800

    nhe/company/.left.menu.php : Fix top menu for NHE company page

---
+++ nhe/company/.left.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Organisationsplan", 
+		"/nhe/company/vis_structure.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Find Medarbejder", 
+		"/nhe/company/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Telefonliste", 
+		"/nhe/company/telephones.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Fødselsdage", 
+		"/nhe/company/birthdays.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
